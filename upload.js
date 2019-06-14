119f8f7445898a2674e9903e1feb39b481609c21
Vincent Tan <vincent+remax@hackcapital.com>
2019-06-14 12:53:03 -0700

    use aws sdk

--- upload.js
+++ upload.js
@@ -1,24 +1,19 @@
 const fs = require('fs');
-const crypto = require('crypto')
 const path = require("path");
+const AWS = require("aws-sdk")
 
 const filePath = path.resolve(process.argv[2])
 var theLog = fs.readFileSync(process.argv[2])
-const bucket = "awsbois"
-const mime = "text/plain"
-var resource = "/" + bucket + "/" + path.basename(filePath, '.log')
-var timestamp = new Date(Date.now())
-console.log(timestamp.toUTCString())
-var toSign = "PUT\n\n" + mime + "\n" + timestamp.toUTCString() + "\n" + resource
 var s3Key = ""
 var s3Secret = ""
+var err = false
 
-var hasher = crypto.createHmac("SHA1", s3Secret)
-hasher.update(theLog)
-var signature = hasher.digest()
-console.log(hasher.digest("hex").toString())
-/*
-sources:
-https://stackoverflow.com/questions/44751574/uploading-to-amazon-s3-via-curl-route/44751929
-//https://stackoverflow.com/questions/31317007/get-a-file-full-path-in-node-js
-*/
+AWS.config.update({ region: 'ca-central-1', accessKeyId: s3Key, secretAccessKey: s3Secret });
+var s3 = new AWS.S3();
+var manUp = s3.upload({ Bucket: 'awsbois', Key: Date.now() + '.log', ACL: "public-read", Body: theLog }, function (err, data) {
+  err = true
+  console.log(err, data)
+})
+
+manUp.send()
+return err ? 1 : 0
