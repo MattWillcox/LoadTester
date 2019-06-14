aa5e78d56532294359271a7e619081d6ca1b50b2
Vincent Tan <vincent+remax@hackcapital.com>
2019-06-14 13:07:01 -0700

    upload.js: converted to module

--- upload.js
+++ upload.js
@@ -1,19 +1,28 @@
-const fs = require('fs');
-const path = require("path");
-const AWS = require("aws-sdk")
+import { readFileSync } from 'fs';
+import { S3, config } from "aws-sdk";
 
-const filePath = path.resolve(process.argv[2])
-var theLog = fs.readFileSync(process.argv[2])
 var s3Key = ""
 var s3Secret = ""
-var err = false
+var buck = 'awsbois'
 
-AWS.config.update({ region: 'ca-central-1', accessKeyId: s3Key, secretAccessKey: s3Secret });
-var s3 = new AWS.S3();
-var manUp = s3.upload({ Bucket: 'awsbois', Key: Date.now() + '.log', ACL: "public-read", Body: theLog }, function (err, data) {
-  err = true
-  console.log(err, data)
-})
+function Upload(filepath) {
+  var err = false
+  var theLog = readFileSync(filepath)
+  var s3 = new S3();
+  var manUp = s3.upload({ Bucket: buck, Key: Date.now() + '.log', ACL: "public-read", Body: theLog }, function (err, data) {
+    err = true
+    console.log(err, data)
+  })
 
-manUp.send()
-return err ? 1 : 0
+  manUp.send()
+  return { err, data }
+}
+
+function Init(key, secret, bucket) {
+  s3Key = key
+  s3Secret = secret
+  buck = bucket
+  config.update({ region: 'ca-central-1', accessKeyId: s3Key, secretAccessKey: s3Secret });
+}
+
+module.exports({ Init: Init(), Upload: Upload() })
