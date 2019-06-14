1e54fb64a494105c551fedaf5b0a126d8ef98a1a
Wesley Mok <wesley@hackcapital.com>
2019-06-14 11:25:05 -0700

    added prompt messages

--- aws-load-tester/index.js
+++ aws-load-tester/index.js
@@ -1,6 +1,12 @@
 const { sdk } = require('@cto.ai/sdk')
 
 async function main() {
+  let instances = prompt("How many instances?: ");
+  let k6GitHub = prompt("What is your github of your k6 script?: ");
+  let virtualUsers = prompt("How many virtual users per instance?: ");
+  let benchMark = prompt("How long do you want to run the benchmark?: ");
+  let writeToS3 = prompt("Do you want to write the results to s3? if so, specify keys: ");
+  let writeToYaml = prompt("Write to yaml: ");
 
 }
 
